d688e7c3b29efd08d1aceedf229138efafa19382
adude900 <yaseentheepic@gmail.com>
2015-08-18 18:23:24 -0400

    Add code
    Added code to the file

---
+++ enable.js
@@ -0,0 +1,10 @@
+//Created on 6/2/15 by Yaseen S.
+
+window.onload = window.setTimeout(
+    function(){
+        console.log("doing something");
+        document.getElementById("storytextp").setAttribute("style", "padding: 0px 0.5em")
+    }, 150);
+
+
+
