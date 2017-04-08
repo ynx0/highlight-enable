f10a71e3f2ff1cea2524c42127adf53358eac72b
Yaseen S <ThePhantomGamer@users.noreply.github.com>
2017-04-08 18:55:57 -0400

    Changed Userscript details to avoid collision

--- enable.user.js
+++ enable.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Fanfiction.net Highlight Enable
+// @name         Fanfiction.net Highlight Enable (test branch)
 // @namespace    https://github.com/ThePhantomGamer/highlight-enable
-// @version      0.1.4
+// @version      0.2.0
 // @description  Lets you highlight things on fanfiction.net
 // @author       Yaseen S./ThePhantomGamer
 // @match        http://www.fanfiction.net/*
